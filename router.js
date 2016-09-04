5b93e002c58a23f303d85cb71fd975e3119d9327
Linus Miller <lohfu@lohfu.io>
2016-09-04 21:27:19 +0200

    Convert routes to router

---
+++ router.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const router = new (require('express')).Router()
+
+const mw = require('./middleware')
+
+const { isAdmin } = require('midwest-module-membership/passport/authorization-middleware')
+
+router.route('/')
+  .get(mw.get)
+  .patch(isAdmin, mw.update)
+
+module.exports = router
